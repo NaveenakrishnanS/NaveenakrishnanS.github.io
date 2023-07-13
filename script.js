07f8bc60e755a2e3b05132bd25571f10abb5c806
S.Naveenakrishnan <infinitetrex0077@gmail.com>
2023-07-13 10:09:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ document.addEventListener("DOMContentLoaded", function () {
     loop: true,
   });
 });
+
+$(document).ready(function() {
+  var slideCount = $(".project-container .project-item").length;
+  var slideWidth = $(".project-container .project-item").outerWidth();
+  var slideContainerWidth = slideCount * slideWidth;
+
+  $(".project-container").css("width", slideContainerWidth);
+
+  var currentPosition = 0;
+  var slideOffset = slideWidth;
+
+  function slideLeft() {
+    if (currentPosition > 0) {
+      currentPosition -= slideOffset;
+      $(".project-container").css("transform", "translateX(-" + currentPosition + "px)");
+    }
+  }
+
+  function slideRight() {
+    if (currentPosition < (slideContainerWidth - slideOffset)) {
+      currentPosition += slideOffset;
+      $(".project-container").css("transform", "translateX(-" + currentPosition + "px)");
+    }
+  }
+
+  $("#prevBtn").click(function() {
+    slideLeft();
+  });
+
+  $("#nextBtn").click(function() {
+    slideRight();
+  });
+});
