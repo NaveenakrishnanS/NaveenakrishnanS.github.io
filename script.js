b985b60979c55188c30f502f27a9734bf9799f28
S.Naveenakrishnan <infinitetrex0077@gmail.com>
2023-07-13 06:59:33 +0530

    portfolio-v1.0

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const typed = new Typed("#typing-heading", {
+    strings: [
+      "NAVEENAKRISHNAN.S",
+      "A Coder",
+      "A Developer",
+      "A Software Engineer",
+    ],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true,
+  });
+});
